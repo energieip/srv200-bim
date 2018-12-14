95d2efed5125fc48c9282c4d0f8c6ce643aea026
Amandine Drebes <amandine@energie-ip.com>
2018-12-14 10:49:53 +0100

    Create a parent object driver

--- js/energieip/drivers/led.js
+++ js/energieip/drivers/led.js
@@ -16,84 +16,18 @@
         xhr.send(JSON.stringify(content));
     }
 
-    energieip.Led = class led extends xeogl.Annotation {
+    energieip.Led = class led extends energieip.Driver {
         get type() {
             return "energieip.Led";
         }
  
-        init(ledObj) {
-            super.init(ledObj);
+        init(sensorObj) {
+            super.init(sensorObj);
             
-            this._groupElement = document.createElement('div');
-            this._groupElement.className = "xeogl-annotation-group";
-            this._groupElement.innerHTML = ledObj.group || "0";
-            this._label.appendChild(this._groupElement);
-
-            this._macElement = document.createElement('div');
-            this._macElement.className = "xeogl-annotation-mac";
-            this._macElement.innerHTML = ledObj.mac || "";
-            this._label.appendChild(this._macElement);
-
-            this._friendlyNameElement = document.createElement('div');
-            this._friendlyNameElement.className = "xeogl-annotation-alias";
-            this._friendlyNameElement.innerHTML = ledObj.friendlyName || "";
-            this._label.appendChild(this._friendlyNameElement);
-
-            this.group = ledObj.group;
-            this.mac = ledObj.mac;
-            this.friendlyName = ledObj.friendlyName;
-            this.label = ledObj.label;
-        }
-
-        set group(gr) {
-            if (this._group === gr) {
-                return;
-            }
-            this._group = gr || "0";
-            this._groupElement.innerHTML = this._group;
-            this.fire("group", this);
-        }
-
-        get group() {
-            return this._group;
-        }
-
-        set mac(mc) {
-            if (this._mac === mc) {
-                return;
-            }
-            this._mac = mc || "";
-            this._macElement.innerHTML = this._mac;
-            this.fire("mac", this);
-        }
-
-        get mac() {
-            return this._mac;
-        }
-
-        set friendlyName(fr) {
-            if (this._friendlyName === fr) {
-                return;
-            }
-            this._friendlyName = fr || "";
-            this._friendlyNameElement.innerHTML = this._friendlyName;
-            this.fire("friendlyName", this);
-        }
-
-        get friendlyName() {
-            return this._friendlyName;
-        }
-    
-        getJSON() {
-            var json = super.getJSON();
-            json.mac = this.mac;
-            json.group = this._group;
-            json.friendlyName = this._friendlyName;
-            return json;
-        }
-
-        destroy() {
-            super.destroy();
+            this._typeElement = document.createElement('div');
+            this._typeElement.className = "xeogl-annotation-group";
+            this._typeElement.innerHTML = "Led";
+            this._label.appendChild(this._typeElement);
         }
     };
 }
