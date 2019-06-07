e90ac8bcd94127597ec83df4da846cdb757502c7
Amandine Drebes <amandine@energie-ip.com>
2019-06-07 13:54:27 +0200

    Add upload mechanism for IFC file

--- src/js/energieip/dashboard.js
+++ src/js/energieip/dashboard.js
@@ -6,6 +6,32 @@ $(document).ready(function() {
     }
 });
 
+
+function CreateButton(name, img, elt, pos, action){
+    btn = document.createElement("input");
+    btn.type = "image";
+    btn.title = name;
+    btn.src = img;
+    btn.alt = name;
+    if (pos != null){
+        btn.style.float = pos;
+    }
+    btn.onclick = action;
+    if (elt != null){
+        placeHolder = document.getElementById(elt);
+    }
+    placeHolder.appendChild(btn);
+}
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function getUserInfo() {
     $.ajax({
         type: "GET",
@@ -30,51 +56,58 @@ function getUserInfo() {
 };
 
 function displayDashboard(priviledge) {
-    logout = document.createElement("input");
-    logout.type = "image";
-    logout.title = "Logout";
-    logout.src = "images/logout.png";
-    logout.alt = "Logout";
-    logout.style.float = "right";
-    // logout.style.left = "100px";
-    logout.onclick = function () {
+    CreateButton("Logout", "images/logout.png", "top", "right", function () {
         window.location.href = 'logout.html';
-    };
-    placeHolder = document.getElementById("top");
-    placeHolder.appendChild(logout);
+    });
 
-    
     if (['admin', 'maintainer'].includes(priviledge)){
-        maintenance = document.createElement("input");
-        maintenance.type = "image";
-        maintenance.title = "Maintenance";
-        maintenance.src = "images/toolbox.png";
-        maintenance.alt = "Maintenance";
-        maintenance.onclick = function () {
+        CreateButton("Maintenance", "images/toolbox.png", "dash", "left", function () {
             window.location.href = 'maintenance.html';
-        };
-        placeHolder = document.getElementById("dash");
-        placeHolder.appendChild(maintenance);
+        });
 
-        stats = document.createElement("input");
-        stats.type = "image";
-        stats.value = "Statistics";
-        stats.src = "images/graph.jpg";
-        stats.onclick = function () {
+        CreateButton("Statistics", "images/graph.jpg", "dash", null, function () {
             window.location.href = 'statistics.html';
-        };
-        placeHolder = document.getElementById("dash");
-        placeHolder.appendChild(stats);
+        });
+
+        CreateButton("Upload", "images/upload.png", "dash", null, function () {
+           modal.style.display='block'
+        });
+
+        $("#upload").click(function() {
+            var fd = new FormData();
+            var files = $('#file')[0].files[0];
+            fd.append('file',files);
+
+            $.ajax({
+                type: "POST",
+                url: energieip.weblink + "map/upload",
+                cache: false,
+                credentials: 'include',
+                data: fd,
+                dataType: 'json',
+                contentType: false,
+                processData: false,
+                crossDomain: true,
+                xhrFields: {
+                    withCredentials: true
+               },
+                statusCode: {
+                    200: function (response) {
+                        alert('file uploaded');
+                    },
+                    401: function (response) {
+                        window.location.href = energieip.loginPage;
+                    },
+                    500: function(response){
+                        alert('file not uploaded');
+                    }
+                },
+            });
+        });
 
     } else {
-        view = document.createElement("input");
-        view.type = "image";
-        view.value = "View";
-        view.src = "images/magnifier.png";
-        view.onclick = function () {
+        CreateButton("View", "images/magnifier.png", "dash", "left", function () {
             window.location.href = 'supervision.html';
-        };
-        placeHolder = document.getElementById("dash");
-        placeHolder.appendChild(view);
+        });
     }
 }
